e122731f6a25be2acea57bb13ad6254805fe8cc6
Kyle Dewey <kyle.dewey@csun.edu>
2020-10-01 16:01:53 -0700

    myBetween works; about to refactor

--- intro_to_recursion/recursion_worksheet.pl
+++ intro_to_recursion/recursion_worksheet.pl
@@ -1,12 +1,38 @@
-factorial(0, 1).
-factorial(Input, Output) :-
+% arity: the number of arguments a procedure takes
+% factorial procedure
+% factorial/2 procedure
+% fact or rule: clause
+factorial(0, 1).            % fact
+factorial(Input, Output) :- % rule
+    % starting point: factorial(Input, Output)
+    % starting point == "head"
     Input >= 1,
     InputMinusOne is Input - 1,
     factorial(InputMinusOne, MinusOneOutput),
     Output is Input * MinusOneOutput.
 
-%% factorial(N, Value):-
-%% 	N>0,
-%% 	NextValue is Value * N,
-%% 	NextN is N -1,
-%% 	factorial(NextN, NextValue).
+% factorial/1 procedure
+factorial(3).
+
+foo(1).
+foo(2).
+foo(X) :-
+    (X = 3; X = 4).
+
+% myBetween(2, 4, X)
+% [2, 3, 4]
+% X = 2
+% 
+
+
+% myBetween(3, 3, X)
+% X = 4
+% myBetween: Low, High, ValueInRange
+myBetween(Low, High, ValueInRange) :-
+    Low =< High,
+    % not done: what is the value?
+    ValueInRange is Low.
+myBetween(Low, High, ValueInRange) :-
+    Low < High,
+    NewLow is Low + 1,
+    myBetween(NewLow, High, ValueInRange).
